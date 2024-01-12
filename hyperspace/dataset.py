5206b9eb787c7df20acdd2a2d5cc7fd12b1f9565
Dariush Bahrami <dariushbahrami1993@gmail.com>
2024-01-12 19:34:49 +0330

    style: rename tensors dataset

--- hyperspace/dataset.py
+++ hyperspace/dataset.py
@@ -2,13 +2,13 @@ import torch
 from torch.utils.data import Dataset
 
 
-class VectorDataset(Dataset):
-    def __init__(self, vectors: torch.Tensor):
+class VectorDatasetFromTensor(Dataset):
+    def __init__(self, vectors: torch.Tensor) -> None:
         assert vectors.ndim == 2
         self.vectors = vectors
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.vectors.shape[0]
 
-    def __getitem__(self, idx):
-        return self.vectors[idx]
+    def __getitem__(self, index: int) -> torch.Tensor:
+        return self.vectors[index]
